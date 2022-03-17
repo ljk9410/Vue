3929bf5c460e05a2864e143ab1005f21e00d0ae4
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2022-03-17 18:02:59 +0900

    new project & vue axios

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Post v-for="(a,i) in postData" :key="i" :post="a" />
+    </div>
+</template>
+
+
+
+<script>
+import Post from './Post.vue';
+
+export default {
+    name: 'container',
+    props: {
+        postData: Array,
+    },
+    components: {
+        Post,
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
